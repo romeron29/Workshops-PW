25f68c00d1721447830f301bcf63395b05eab3eb
romeron29 <nes.romero.17@gmail.com>
2024-09-30 15:18:37 -0600

    First commit}

---
+++ firstworkshop/index.php
@@ -0,0 +1,16 @@
+<?php
+// Set the default timezone to use. 
+// You can change this to your preferred timezone, e.g., 'America/New_York'.
+date_default_timezone_set("America/Costa_Rica");
+
+// Get the current date and time.
+$currentDateTime = date('Y-m-d H:i:s');
+
+// Print the current date and time.}
+
+echo "Current date and time: " . $currentDateTime;
+
+$number = 2;
+$tot = $number + "2 a";
+echo "total  $tot";
+?>

2442056f30bd7ca9c2fb4da73ee89feb2fd26e30
romeron29 <146119486+romeron29@users.noreply.github.com>
2024-09-30 17:40:24 -0600

    Delete Second Workshop directory

--- Second Workshop/secondworkshop/printtest.php
+++
@@ -1,7 +0,0 @@
-<?php
-$name = $_REQUEST["name"];
-$lastname  = $_REQUEST["lastname"];
-$phonenumber = $_REQUEST["phonenumber"];
-$email = $_REQUEST["email"];
-
-echo $name," ",$lastname," ", $phonenumber," ",$email;
